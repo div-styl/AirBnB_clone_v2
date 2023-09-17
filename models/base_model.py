5e8911593042a513ec48831643dfa8c4f1b9c4a7
adil_ma <itsadilhadid@gmail.com>
2023-09-17 16:51:49 +0100

    setting up sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""BaseModel class"""
+""" basemodel the mother of all models """
 
 import models
 from uuid import uuid4
 from datetime import datetime
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
 from sqlalchemy import DateTime
@@ -13,16 +14,15 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """BaseModel class
-    """
+    """ the base model which all other models inherit from """
+
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-
     def __init__(self, *args, **kwargs):
-        """baseModel constructor
-        """
+        """ base model constructor """
+
         self.id = str(uuid4())
         self.created_at = self.updated_at = datetime.utcnow()
         if kwargs:
@@ -33,14 +33,14 @@ class BaseModel:
                     setattr(self, key, value)
 
     def save(self):
-        """Update updated_at"""
+        """ update the last updated time to now """
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """Return a dictionary representation of the BaseModel
-        """
+        """ update the last updated time to now """
+        
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
@@ -49,11 +49,11 @@ class BaseModel:
         return my_dict
 
     def delete(self):
-        """Delete instance from storage"""
+        """delete the current instance"""
         models.storage.delete(self)
 
     def __str__(self):
-        """Return string reperesentation of BaseModel"""
+        """ class string representation """
         d = self.__dict__.copy()
         d.pop("_sa_instance_state", None)
         return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
