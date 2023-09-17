f0a18ce8d339a70fe9cb94191e8aa6a877945f1f
adil_ma <itsadilhadid@gmail.com>
2023-09-17 16:56:32 +0100

    setting up sqlalchemy

--- models/__init__.py
+++ models/__init__.py
@@ -1,20 +1,13 @@
+
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from os import getenv
-
-
-storage = FileStorage()
-storage.reload()
-
-
-
 """initiat a storage object."""
+import os
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+
+storage = DBStorage() if os.getenv(
+    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+
+
 storage.reload()
