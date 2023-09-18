32710e490bfac526a336de59451dc05f4c4cbb34
adil_ma <itsadilhadid@gmail.com>
2023-09-18 14:19:14 +0100

    task 7

--- models/user.py
+++ models/user.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 """Defines the User class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
-    """Represents a user in db
-    """
-    __tablename__ = "users"
+    """Represents a user."""
+
+    __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
