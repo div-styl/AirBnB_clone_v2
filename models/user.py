42c9c35dd8ddd9d734167b11e75539b333ceca2e
adil_ma <itsadilhadid@gmail.com>
2023-09-18 14:11:37 +0100

    task 7

--- models/user.py
+++ models/user.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+""" holds class User"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class User(BaseModel, Base):
-    """Represents a user."""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        places = relationship("Place", backref="user", cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initialize user"""
+        super().__init__(*args, **kwargs)
