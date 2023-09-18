e9a559d3dfe6343954d7c5104ec1ad4c293c9fe3
adil_ma <itsadilhadid@gmail.com>
2023-09-18 14:06:41 +0100

    task 7

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Defines the User class."""
-
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """Represents a user."""
@@ -11,3 +11,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship("Place", backref="user", cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
