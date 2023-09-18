4afde5f235a4b785582bb36bd567a830aaf39d69
adil_ma <itsadilhadid@gmail.com>
2023-09-18 16:46:01 +0100

    fixing ruined code

--- models/place.py
+++ models/place.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
-class Place(BaseModel, Base):
-    """Represents a place."""
 
-    __tablename__ = 'places'
+class Place(BaseModel):
+    """Represent a place.
+    attributes:
+        city_id (str): city id
+        user_id (str): user id
+        name (str): name of the place
+        description (str): description of the place
+        number_rooms (int): number of rooms
+        number_bathrooms (int): number of bathrooms
+        max_guest (int): max number of guests
+        price_by_night (int): price by night
+        latitude (float): latitude
+        longitude (float): longitude
+        amenity_ids (list): list of amenity ids
+    """
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-
-    # Add a relationship with User class
-    user = relationship("User", backref="places")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
