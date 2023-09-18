81d3c4fed919d625d252eb6b879d03986f7ce6bb
adil_ma <itsadilhadid@gmail.com>
2023-09-18 14:20:46 +0100

    task 7

--- models/user.py
+++ models/user.py
@@ -12,3 +12,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
