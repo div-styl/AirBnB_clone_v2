9cf30b53cdba08119db46e39775f819bfc3c1f6c
div-styl <gouhmada@gmail.com>
2023-09-19 23:55:29 +0100

    updated

--- models/user.py
+++ models/user.py
@@ -1,42 +1,35 @@
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
+#!/usr/bin/python3
+"""This is the user class"""
+import models
+
+from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
 from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
-class Place(BaseModel, Base):
-    """Represent a place."""
-    __tablename__ = "places"
 
+class User(BaseModel, Base):
+    """User class inherits from BaseModel
+    Attributes:
+        email (str): email address
+        password (str): password
+        first_name (str): first name
+        last_name (str): last name
+    """
+
+    __tablename__ = "users"
     if storage_type == "db":
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, default=0, nullable=False)
-        number_bathrooms = Column(Integer, default=0, nullable=False)
-        max_guest = Column(Integer, default=0, nullable=False)
-        price_by_night = Column(Integer, default=0, nullable=False)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place", cascade="all, delete")
-        amenities = relationship(
-            "Amenity",
-            secondary="place_amenity",
-            viewonly=False,
-            back_populates="place_amenities"
-        )
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", cascade="all, delete" ,backref="user")
+        review = relationship("Review", cascade="all, delete" ,backref="user")
     else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
-
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
