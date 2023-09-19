3c29cb0cb468293914a95da57ba967800b58a950
div-styl <gouhmada@gmail.com>
2023-09-19 21:17:15 +0100

    relation to review

--- models/place.py
+++ models/place.py
@@ -43,16 +43,17 @@ class Place(BaseModel):
     """
 
     if models.storage_type == "db":
-        city_id = Column(String(60), ForeignKey("cities.id") ,nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id") ,nullable=False)
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, default=0 ,nullable=True)
-        number_bathrooms = Column(Integer, default=0 ,nullable=True)
-        max_guest = Column(Integer, default=0 ,nullable=True)
-        price_by_night = Column(Integer, default=0 ,nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=True)
+        number_bathrooms = Column(Integer, default=0, nullable=True)
+        max_guest = Column(Integer, default=0, nullable=True)
+        price_by_night = Column(Integer, default=0, nullable=True)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        review = relationship("Review", backref="place")
     else:
         city_id = ""
         user_id = ""
@@ -65,3 +66,6 @@ class Place(BaseModel):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
