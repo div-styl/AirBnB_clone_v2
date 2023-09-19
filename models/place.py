ba24173822fa85ef2e348765b3a188e00f3adf24
div-styl <gouhmada@gmail.com>
2023-09-19 20:34:17 +0100

    some major adjustments into folder structure and adding tables into models

--- models/place.py
+++ models/place.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3
+import models
+
 from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from os import getenv
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+if models.storage_type == "db":
+    place_amenity = Table(
+        "place_amenity",
+        Base.metadata,
+        Column(
+            "place_id",
+            String(60),
+            ForeignKey("places.id", ondelete="CASCADE", onupdate="CASCADE"),
+            primary_key=True,
+        ),
+        Column(
+            "amenity_id",
+            String(60),
+            ForeignKey("amenities.id", ondelete="CASCADE", onupdate="CASCADE"),
+            primary_key=True,
+        ),
+    )
 
 
 class Place(BaseModel):
@@ -18,14 +42,26 @@ class Place(BaseModel):
         amenity_ids (list): list of amenity ids
     """
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    if models.storage_type == "db":
+        city_id = Column(String(60), ForeignKey("cities.id") ,nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id") ,nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0 ,nullable=True)
+        number_bathrooms = Column(Integer, default=0 ,nullable=True)
+        max_guest = Column(Integer, default=0 ,nullable=True)
+        price_by_night = Column(Integer, default=0 ,nullable=True)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
--- models/user.py
+++ models/user.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
+import models
+
 from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from os import getenv
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
@@ -11,8 +17,18 @@ class User(BaseModel):
         first_name (str): first name
         last_name (str): last name
     """
+    if models.storage_type == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user")
+        review = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    
