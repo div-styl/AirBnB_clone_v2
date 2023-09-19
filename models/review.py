34782d33c0e24a46c8091d2a2b2d834425afcda7
div-styl <gouhmada@gmail.com>
2023-09-19 23:55:55 +0100

    the s is added

--- models/review.py
+++ models/review.py
@@ -13,7 +13,7 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 class Review(BaseModel, Base):
     """ Review classto store review information """
 
-    __tablename__ = "Review"
+    __tablename__ = "Reviews"
     if storage_type == "db":
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
