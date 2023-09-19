4d14ad8f183e6f6eba4f60ba5a2132dd97833d85
adil_ma <itsadilhadid@gmail.com>
2023-09-19 14:53:41 +0100

    task 5

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,11 @@
 """ Initialize the storage module """
 from os import getenv
 
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
