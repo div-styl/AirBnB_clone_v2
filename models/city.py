b06a6077a65380a22569c93aca5bfec42b0d6cf9
adil_ma <itsadilhadid@gmail.com>
2023-09-19 23:06:18 +0100

    task 5

--- models/city.py
+++ models/city.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
 """Defines the City class."""
-import models
-
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
 
 class City(BaseModel, Base):
     """Represent a city."""
-
-    if models.storage_type == "db":
-        __tablename__ = "cities"
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship("Place", backref="cities")
-    else:
-        state_id = ""
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
