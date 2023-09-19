8b47b199cdecaff2f2b6789843a46737f65978ad
adil_ma <itsadilhadid@gmail.com>
2023-09-19 23:49:19 +0100

    task 8

--- models/city.py
+++ models/city.py
@@ -1,21 +1,18 @@
-#!/usr/bin/python3
-"""Defines the City class."""
-import models
-
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
 class City(BaseModel, Base):
     """Represent a city."""
-
     __tablename__ = "cities"
+
     if storage_type == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship("Place", backref="cities")
+        places = relationship("Place", backref="cities", cascade="all, delete")
     else:
         state_id = ""
         name = ""
--- models/user.py
+++ models/user.py
@@ -1,33 +1,21 @@
-#!/usr/bin/python3
-"""This is the user class"""
-import models
-
-from models.base_model import BaseModel
-from models.base_model import Base, BaseModel
-from os import getenv
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
-
 class User(BaseModel, Base):
-    """User class inherits from BaseModel
-    Attributes:
-        email (str): email address
-        password (str): password
-        first_name (str): first name
-        last_name (str): last name
-    """
-
+    """User class inherits from BaseModel"""
     __tablename__ = "users"
+
     if storage_type == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", cascade="all, delete" ,backref="user")
-        review = relationship("Review", cascade="all, delete" ,backref="user")
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user", cascade="all, delete")
     else:
         email = ""
         password = ""
