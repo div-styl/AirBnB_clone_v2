0b1a9e7cf4dff20f5b5dee43a5803f2e97d58d05
adil_ma <itsadilhadid@gmail.com>
2023-09-19 15:39:50 +0100

    task 7

--- models/user.py
+++ models/user.py
@@ -4,9 +4,9 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
 class User(BaseModel, Base):
-  """Represent a user."""
-  __tablename__ = 'users'
-  email = Column(String(128), notnull=True)
-  password = Column(String(128), notnull=True)
-  first_name = Column(String(128), nullable=True)
-  last_name = Column(String(128), nullable=True)
+    """Represent a user."""
+    __tablename__ = 'users'
+    email = Column(String(128), notnull=True)
+    password = Column(String(128), notnull=True)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
