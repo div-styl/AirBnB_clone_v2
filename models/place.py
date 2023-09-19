3047bdda6b92fa9750f27e823bf7d5d6d9902539
adil_ma <itsadilhadid@gmail.com>
2023-09-19 23:51:26 +0100

    task 8

--- models/place.py
+++ models/place.py
@@ -1,14 +1,51 @@
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
+#!/usr/bin/python3
+import models
 
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+from os import getenv
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
-class Place(BaseModel, Base):
-    """Represent a place."""
-    __tablename__ = "places"
 
+if storage_type == "db":
+    place_amenity = Table(
+        "place_amenity",
+        Base.metadata,
+        Column(
+            "place_id",
+            String(60),
+            ForeignKey("places.id", ondelete="CASCADE", onupdate="CASCADE"),
+            primary_key=True,
+            nullable=False
+        ),
+        Column(
+            "amenity_id",
+            String(60),
+            ForeignKey("amenities.id", ondelete="CASCADE", onupdate="CASCADE"),
+            primary_key=True,
+            nullable=False
+        ),
+    )
+
+
+class Place(BaseModel, Base):
+    """Represent a place.
+    attributes:
+        city_id (str): city id
+        user_id (str): user id
+        name (str): name of the place
+        description (str): description of the place
+        number_rooms (int): number of rooms
+        number_bathrooms (int): number of bathrooms
+        max_guest (int): max number of guests
+        price_by_night (int): price by night
+        latitude (float): latitude
+        longitude (float): longitude
+        amenity_ids (list): list of amenity ids
+    """
+    __tablename__ = "places"
     if storage_type == "db":
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
@@ -20,12 +57,12 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place", cascade="all, delete")
+        reviews = relationship("Review", backref="place")
         amenities = relationship(
             "Amenity",
-            secondary="place_amenity",
+            secondary=place_amenity,
+            back_populates="place_amenities",
             viewonly=False,
-            back_populates="place_amenities"
         )
     else:
         city_id = ""
@@ -40,3 +77,28 @@ class Place(BaseModel, Base):
         longitude = 0.0
         amenity_ids = []
 
+
+
+        @property
+        def review(self):
+            """getter for review"""
+            from models.review import Review
+
+            review_list = []
+            all_rev = models.storage.all(Review)
+            for review in all_rev.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+        @property
+        def amenities(self):
+            """getter for amenities"""
+            from models.amenity import Amenity
+
+            amenity_list = []
+            all_amen = models.storage.all(Amenity)
+            for amenity in all_amen.values():
+                if amenity.place_id == self.id:
+                    amenity_list.append(amenity)
+            return amenity_list
--- models/user.py
+++ models/user.py
@@ -1,23 +1,42 @@
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
-class User(BaseModel, Base):
-    """User class inherits from BaseModel"""
-    __tablename__ = "users"
+class Place(BaseModel, Base):
+    """Represent a place."""
+    __tablename__ = "places"
 
     if storage_type == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade="all, delete")
-        reviews = relationship("Review", backref="user", cascade="all, delete")
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place", cascade="all, delete")
+        amenities = relationship(
+            "Amenity",
+            secondary="place_amenity",
+            viewonly=False,
+            back_populates="place_amenities"
+        )
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
