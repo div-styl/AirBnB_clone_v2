71d6fbd2452acf03991b5fee7395e311b6deff59
div-styl <gouhmada@gmail.com>
2023-10-05 21:52:37 +0100

    Update!

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""pack and deploy content to server
+""" module doc
 """
-from fabric.api import local, env, run, put
+from fabric.api import task, local, env, put, run, runs_once
 from datetime import datetime
 import os
 
-env.hosts = ["54.90.63.243", "54.90.4.180"]
-
-env.user = "ubuntu"
+env.hosts = ['54.90.63.243', '54.90.4.180']
 
 
+@runs_once
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
@@ -30,68 +29,47 @@ def do_pack():
         return None
 
 
+@task
 def do_deploy(archive_path):
-    """Deploy package to remote server.
-
-    Args:
-        archive_path (str): Path to archive to deploy.
-
-    Returns:
-        bool: True if deployment is successful, False otherwise.
     """
-    # Check if archive_path is valid
-    if not archive_path or not os.path.exists(archive_path):
-        return False
-
-    # Copy the archive to /tmp
-    put(archive_path, "/tmp")
-
-    # Extract the name of the archive
-    ar_name = archive_path[archive_path.find("/") + 1: -4]
-
+    Deploys a web static package to the web server.
+    
+    Args:
+        archive_path (str): The path to the web static package archive.
+        
+    Returns:
+        bool: True if the deployment is successful, False otherwise.
+    """
     try:
-        # Create the necessary release directory
-        run("mkdir -p /data/web_static/releases/{}/".format(ar_name))
-
-        # Extract the archive to the release directory
-        run(
-            "tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(
-                ar_name, ar_name
-            )
-        )
-
-        # Remove the archive from /tmp
-        run("rm /tmp/{}.tgz".format(ar_name))
-
-        # Move the contents of the web_static folder to the release directory
-        run(
-            "mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(ar_name, ar_name)
-        )
-
-        # Remove the empty web_static folder
-        run("rm -rf /data/web_static/releases/{}/web_static".format(ar_name))
-
-        # Remove the current symlink
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
         run("rm -rf /data/web_static/current")
-
-        # Create a new symlink to the latest release
-        run(
-            "ln -s /data/web_static/releases/{}/ "
-            "/data/web_static/current".format(ar_name)
-        )
-
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
         print("New version deployed!")
         return True
     except Exception:
         return False
 
 
+@task
 def deploy():
     """
-    function to pack and deploy
+    A function that deploys something.
+
+    Returns:
+        bool: True if the deployment was successful, False otherwise.
     """
-    pack = do_pack()
-    if pack:
-        return do_deploy(pack)
-    return False
+    path = do_pack()
+    if path is None:
+        return False
+    return do_deploy(path)
