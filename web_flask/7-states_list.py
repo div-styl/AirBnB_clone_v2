7f7df5000806896ab1a27c3724108c0093b654d9
div-styl <gouhmada@gmail.com>
2023-10-20 10:55:41 +0100

    :bug: some code style fixed and :rocket: new html file are deployed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
 """list the states of my database"""
 
-from cgitb import strong
-from email.policy import strict
+
 from flask import Flask, render_template
 from models import *
 from models import storage
+
 app = Flask(__name__)
+
 
 @app.route("/states_list", strict_slashes=False)
 def state_list():
-    """"""
-    states = sorted(list(storage.all('State').values()), key=lambda x:x.name)
-    return render_template('7-states_list.html', states=states)
+    """
+    Route handler for displaying a list of states.
+
+    Returns:
+        A rendered HTML template with a sorted list of states.
+    """
+
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+
+    return render_template("7-states_list.html", states=states)
+
 
 @app.teardown_appcontext
 def tear_db(exception):
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
