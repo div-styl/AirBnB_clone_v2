68bdc0147da8dcb47db14c821b7cce264105d401
div-styl <gouhmada@gmail.com>
2023-10-21 13:24:15 +0100

    pyc

--- models/state.py
+++ models/state.py
@@ -9,15 +9,17 @@ from sqlalchemy.orm import relationship
 from os import getenv
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
+
 class State(BaseModel, Base):
     """Represent a state."""
     __tablename__ = 'states'
 
     if storage_type == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
     else:
-        name=""
+        name = ""
 
         @property
         def cities(self):
