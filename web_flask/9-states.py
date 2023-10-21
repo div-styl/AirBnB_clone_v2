11195d0b5b78fb9e8a669ea1028305b488ea685a
div-styl <gouhmada@gmail.com>
2023-10-21 19:39:12 +0100

    Update!

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""
+start a flask app
+"""
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<state_id>", strict_slashes=False)
+def states(state_id=None):
+    states = storage.all(State)
+    if state_id is not None:
+        state_id = "State." + state_id
+    return render_template("9-states.html", states=states, state_id=state_id)
+
+
+@app.teardown_appcontext
+def tear_db(exception):
+    """
+    Teardown function to close the database connection.
+
+    Args:
+        exception (Exception): The exception raised, if any.
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
