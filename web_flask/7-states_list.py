ea0f9b79e28bcf7c8c01ebaf54b3e2d2b4dd3483
div-styl <gouhmada@gmail.com>
2023-10-21 13:08:41 +0100

    Update!

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,9 +5,92 @@
 from flask import Flask, render_template
 from models import *
 from models import storage
+import models
+from models import state
 
 app = Flask(__name__)
 
+app.route("/", strict_slashes=False)
+def index():
+    """retunr Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """retunr HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    Endpoint that returns 'C' followed by the provided
+    text, with underscores replaced by spaces.
+
+    Parameters:
+        text (str): The text to be included after 'C'.
+
+    Returns:
+        str: The concatenation of 'C' and the modified text.
+    """
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythoniscool(text="is cool"):
+    """
+    Endpoint that returns 'Python' followed by the provided
+    text, with underscores replaced by spaces.
+
+    Args:
+        text (str, optional): The text to append
+            after 'Python'. Defaults to "is cool".
+
+    Returns:
+        str: The final string with 'Python' and the provided text.
+    """
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    A route that returns a string representation of the given number.
+
+    Args:
+        n (int): The number to be converted to a string.
+
+    Returns:
+        str: The string representation of the given number.
+    """
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def num_tamplate(n):
+    """
+    This function handles a GET request
+        to the /number_template/<int:n> endpoint.
+
+    Parameters:
+        n (int): The number specified in the URL path.
+
+    Returns:
+        str: The rendered HTML template with the number passed as a parameter.
+    """
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_num(n):
+    if (n % 2) == 0:
+        evenness = "even"
+    else:
+        evenness = "odd"
+    return render_template("6-number_odd_or_even.html", n=n,
+                           even_or_odd=evenness)
 
 @app.route("/states_list", strict_slashes=False)
 def state_list():
@@ -17,8 +100,8 @@ def state_list():
     Returns:
         A rendered HTML template with a sorted list of states.
     """
-
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    # sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = storage.all(state)
 
     return render_template("7-states_list.html", states=states)
 
