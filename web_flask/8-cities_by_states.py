b17bd9edc87acceeb2cb38bc05de59d89a8f9edd
div-styl <gouhmada@gmail.com>
2023-10-22 11:40:43 +0100

    task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,118 +10,6 @@ from models import state
 app = Flask(__name__)
 
 
-app.route("/", strict_slashes=False)
-
-
-def index():
-    """retunr Hello HBNB"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """retunr HBNB"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """
-    Endpoint that returns 'C' followed by the provided
-    text, with underscores replaced by spaces.
-
-    Parameters:
-        text (str): The text to be included after 'C'.
-
-    Returns:
-        str: The concatenation of 'C' and the modified text.
-    """
-    return "C " + text.replace("_", " ")
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def pythoniscool(text="is cool"):
-    """
-    Endpoint that returns 'Python' followed by the provided
-    text, with underscores replaced by spaces.
-
-    Args:
-        text (str, optional): The text to append
-            after 'Python'. Defaults to "is cool".
-
-    Returns:
-        str: The final string with 'Python' and the provided text.
-    """
-    return "Python " + text.replace("_", " ")
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    """
-    A route that returns a string representation of the given number.
-
-    Args:
-        n (int): The number to be converted to a string.
-
-    Returns:
-        str: The string representation of the given number.
-    """
-    return f"{n} is a number"
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def num_tamplate(n):
-    """
-    This function handles a GET request
-        to the /number_template/<int:n> endpoint.
-
-    Parameters:
-        n (int): The number specified in the URL path.
-
-    Returns:
-        str: The rendered HTML template with the number passed as a parameter.
-    """
-    return render_template("5-number.html", n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def odd_num(n):
-    if (n % 2) == 0:
-        evenness = "even"
-    else:
-        evenness = "odd"
-    return render_template("6-number_odd_or_even.html",
-                           n=n, even_or_odd=evenness)
-
-
-app.route("/states_list", strict_slashes=False)
-
-
-def state_list():
-    """
-    Route handler for displaying a list of states.
-
-    Returns:
-        A rendered HTML template with a sorted list of states.
-    """
-    # sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    states = storage.all(state)
-
-    return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def tear_db(exception):
-    """
-    Teardown function to close the database connection.
-
-    Args:
-        exception (Exception): The exception raised, if any.
-    """
-    storage.close()
-
-
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
     """Display a list of states and render it as an HTML template.
@@ -135,6 +23,17 @@ def cities_by_states():
 
     # Render the HTML template with the states
     return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def tear_db(exception):
+    """
+    Teardown function to close the database connection.
+
+    Args:
+        exception (Exception): The exception raised, if any.
+    """
+    storage.close()
 
 
 if __name__ == "__main__":
